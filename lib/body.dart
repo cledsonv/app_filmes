e34a0f0f67f71cfa290da8c679f50acbb694c486
João bosco <junior232.jb@gmail.com>
2022-09-26 13:12:10 -0300

    ImagensFilmes

--- lib/body.dart
+++ lib/body.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:testando_widgets/components/constant.dart';
+import 'package:testando_widgets/home_page/components_home_page/imagens_filmes.dart';
 import 'package:testando_widgets/home_page/home_page.dart';
 
 class PrincipalBody extends StatefulWidget {
@@ -12,9 +14,6 @@ class PrincipalBody extends StatefulWidget {
 class _PrincipalBodyState extends State<PrincipalBody> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      
-      body: const HomePage(),
-    );
+    return const HomePage();
   }
 }
--- lib/home_page/home_page.dart
+++ lib/home_page/home_page.dart
@@ -1,17 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:testando_widgets/home_page/components_home_page/imagens_filmes.dart';
 import '../components/constant.dart';
 import 'components_home_page/bottom_navigator_page.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  const HomePage({super.key, required});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       backgroundColor: kPrimaryColor,
-      bottomNavigationBar: 
-      
-      NavigatorPage(),
+      bottomNavigationBar: NavigatorPage(),
+      body: ImagensFilmes(),
     );
   }
 }
